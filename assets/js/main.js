f0ce756bd2771d234920d3c037aa9efd0e572544
LucianoBrumer <lucianobrumer5@gmail.com>
2023-04-25 00:42:04 -0300

    working all

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,6 +5,11 @@ const mainElement = document.querySelector('.main')
 const addElement = document.querySelector('.add')
 const removeElement = document.querySelector('.remove')
 
+const totalCostoElement = document.querySelector('.total-costo')
+const totalManoElement = document.querySelector('.total-mano')
+const totalPropElement = document.querySelector('.total-prop')
+const totalBrutoElement = document.querySelector('.total-bruto')
+
 const costoCallback = event => {
     event.target.parentNode.querySelector('.bruto').value = event.target.value - event.target.parentNode.querySelector('.mano').value
     event.target.parentNode.querySelector('.prop').value = (event.target.parentNode.querySelector('.mano').value * 100) / event.target.value
@@ -13,6 +18,15 @@ const costoCallback = event => {
     }else{
         event.target.parentNode.querySelector('.bruto').classList.remove('text-red-600')
     }
+
+    let totalCosto = 0
+    document.querySelectorAll('.costo').forEach(costoElement => {
+        if(costoElement.value.length > 0) totalCosto = parseFloat(totalCosto) + parseFloat(costoElement.value)
+    })
+    totalCostoElement.innerHTML = totalCosto
+
+    totalPropElement.innerHTML = (parseFloat(totalManoElement.innerHTML) * 100) / parseFloat(totalCostoElement.innerHTML)
+    totalBrutoElement.innerHTML = parseFloat(totalCostoElement.innerHTML) - parseFloat(totalManoElement.innerHTML)
 }
 
 const manoCallback = event => {
@@ -26,6 +40,15 @@ const manoCallback = event => {
         event.target.parentNode.querySelector('.bruto').classList.remove('text-red-600')
         event.target.parentNode.querySelector('.bruto').classList.add('text-green-600')
     }
+
+    let totalMano = 0
+    document.querySelectorAll('.mano').forEach(manoElement => {
+        if(manoElement.value.length > 0) totalMano = parseFloat(totalMano) + parseFloat(manoElement.value)
+    })
+    totalManoElement.innerHTML = totalMano
+
+    totalPropElement.innerHTML = (parseFloat(totalManoElement.innerHTML) * 100) / parseFloat(totalCostoElement.innerHTML)
+    totalBrutoElement.innerHTML = parseFloat(totalCostoElement.innerHTML) - parseFloat(totalManoElement.innerHTML)
 }
 
 costoElements.forEach(costoElement => {
