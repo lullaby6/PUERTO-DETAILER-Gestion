3246454e4026b2b427ea4d42612bc791751c76e4
LucianoBrumer <lucianobrumer5@gmail.com>
2023-04-25 00:16:12 -0300

    cliente props

--- assets/js/main.js
+++ assets/js/main.js
@@ -4,12 +4,9 @@ const manoElements = document.querySelectorAll('.mano')
 const mainElement = document.querySelector('.main')
 const addElement = document.querySelector('.add')
 
-filasElements.forEach(filaElement => {
-
-})
-
 const costoCallback = event => {
     event.target.parentNode.querySelector('.bruto').value = event.target.value - event.target.parentNode.querySelector('.mano').value
+    event.target.parentNode.querySelector('.prop').value = (event.target.parentNode.querySelector('.mano').value * 100) / event.target.value
     if (event.target.parentNode.querySelector('.bruto').value < 0){
         event.target.parentNode.querySelector('.bruto').classList.add('text-red-600')
     }else{
@@ -19,6 +16,8 @@ const costoCallback = event => {
 
 const manoCallback = event => {
     event.target.parentNode.querySelector('.bruto').value = event.target.parentNode.querySelector('.costo').value - event.target.value
+    event.target.parentNode.querySelector('.prop').value = (event.target.value * 100) / event.target.parentNode.querySelector('.costo').value
+
     if (event.target.parentNode.querySelector('.bruto').value < 0){
         event.target.parentNode.querySelector('.bruto').classList.add('text-red-600')
         event.target.parentNode.querySelector('.bruto').classList.remove('text-green-600')
@@ -36,14 +35,23 @@ manoElements.forEach(manoElement => {
     manoElement.addEventListener('input', event => manoCallback(event))
 })
 
-addElement.addEventListener('click', event => {
+addElement.addEventListener('click', () => {
     const newFilaElement = filasElements[0].cloneNode(true)
     newFilaElement.childNodes.forEach(newFilaElementChild => {
         newFilaElementChild.value = ''
         try {
+            if(newFilaElementChild.className.includes('zero')) newFilaElementChild.value = '0'
             if(newFilaElementChild.className.includes('costo')) newFilaElementChild.addEventListener('input', event => costoCallback(event))
             if(newFilaElementChild.className.includes('mano')) newFilaElementChild.addEventListener('input', event => manoCallback(event))
         } catch (error) {}
     })
     mainElement.appendChild(newFilaElement)
+})
+
+console.log(document.querySelector('.date-now'));
+
+document.querySelectorAll('.date-now').forEach(dateNowElement => {
+    let now = new Date();
+    now.setMinutes(now.getMinutes() - now.getTimezoneOffset());
+    dateNowElement.value = now.toISOString().slice(0, 16);
 })
